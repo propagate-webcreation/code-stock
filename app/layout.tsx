5170ae6d587b427c755265c6dbe845f77e822955
propagate1 <info@propagateinc.com>
2026-04-19 16:04:41 +0900

    feat: GTM コンテナ + data 属性を設置
    - GTM ID: GTM-W98RVBKZ
    - 測定ID: G-4CSZWHZLX1
    - 旧 gtag.js/CVR スクリプトを GTM 方式へ移行

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,14 @@ export default function RootLayout({
       <body
         className="antialiased font-body"
       >
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W98RVBKZ');`}
+        </Script>
+      
         {children}
         <Script
           src="https://site-annotator.vercel.app/tracker.js"
